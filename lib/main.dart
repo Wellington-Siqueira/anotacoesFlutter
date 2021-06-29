95c104d8617cff20a708d07e115bfe7bef72b1e7
Wellington Guimaraes Siqueira <wellingtonsiqueira333@gmail.com>
2021-06-29 09:28:37 -0300

    Update para orientação de tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'Homepage.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
